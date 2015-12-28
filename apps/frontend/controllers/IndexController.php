718049fd248721984548301ece83db168f2ddc73
yan_kos <yankos.kos@gmail.com>
2015-12-28 16:44:52 +0200

    ADD basic conrtoller

---
+++ apps/frontend/controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Frontend\Controllers;
+
+use \Phalcon\Mvc\Controller,
+	\Phalcon\Mvc\View,
+	\Phalcon\Http\Response;
+
+class IndexController extends Controller
+{
+	public function indexAction()
+	{
+
+	}
+}
